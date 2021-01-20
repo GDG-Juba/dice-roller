50395dba4ae295f8b6dc3fb69a1c24ae559fd93b
kose-uk45 <kosebilaliuk45@gmail.com>
2021-01-20 15:25:43 +0200

    day 2 dice roller

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dice-roller
-Android Study Jams- dice roller app on the second Saturday of the weekily study jam
+## Android Study Jams- dice roller app on the second Saturday of the weekily study jam
+##  Day 2
---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val resultTextView: TextView = findViewById(R.id.roll_text)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.roll_text)
+        resultTextView.text = diceRoll.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // update appropriate image for the roll number
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
